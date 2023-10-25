38b95f56a24891405cf4bc3f8aa6c47a724a8fb8
jeongeun <wjddms9921@gmail.com>
2023-10-26 01:58:10 +0900

    ADD : Home 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
-import SignupStep1 from "./pages/Signup/SignupStep1/index.tsx"
+import logo from "./logo.svg";
+import "./App.css";
+import SignupStep1 from "./pages/Signup/SignupStep1/index.tsx";
 import SignupStep2 from "./pages/Signup/SignupStep2/index.tsx";
 import Login from "./pages/Login/index.tsx";
+import Home from "./pages/Home/index.tsx";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signupstep1" element={<SignupStep1 />} />
-        <Route path="/signupstep2" element={<SignupStep2/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/signupstep2" element={<SignupStep2 />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
