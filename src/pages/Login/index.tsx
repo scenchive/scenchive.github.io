cf30a70ce29d6968cff03cf792c5a1a61fdc456f
VanesaK <minitea@sookmyung.ac.kr>
2023-11-28 15:54:08 +0900

    [ADD] 마이페이지 추가

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -28,27 +28,29 @@ const Login = () => {
     navigate(-1);
   }
   const Login = async () => {
-    if (email.length>0 && password.length>0){
-    const data={
-      email:email,
-      password:password,
-    }
-    await axios.post('/login', data)
-    .then((res)=>{
-      console.log(res)
-      if (res.data.token){
-        console.log('로그인 성공했습니다.')
+    if (email.length > 0 && password.length > 0) {
+      const data = {
+        email: email,
+        password: password,
       }
-      goToHome();
-    })
-  }else{
-    alert("모든 항목을 입력해주세요.")
-  }
+      console.log('data', data)
+      await axios.post('/login', data)
+        .then((res) => {
+          if (res.data.token) {
+            console.log('로그인 성공했습니다.')
+            localStorage.setItem('my-token',res.data.token)
+            goToHome();
+          }
+
+        })
+    } else {
+      alert("모든 항목을 입력해주세요.")
+    }
   }
 
 
-  const handleOnKeyPress=(e: { key: string; })=>{
-    if (e.key==='Enter'){
+  const handleOnKeyPress = (e: { key: string; }) => {
+    if (e.key === 'Enter') {
       Login();
     }
   }
@@ -61,11 +63,15 @@ const Login = () => {
         <LoginArea>
           <RowArea>
             <QuestionRow>이메일</QuestionRow>
-            <AnswerRow type="text" onChange={(e: { target: { value: React.SetStateAction<string>; }; })=>setEmail(e.target.value)} onKeyPress={handleOnKeyPress} />
+            <AnswerRow type="text"
+             placeholder="이메일을 입력해주세요."
+            onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setEmail(e.target.value)} onKeyPress={handleOnKeyPress} />
           </RowArea>
           <RowArea>
             <QuestionRow>비밀번호</QuestionRow>
-            <AnswerRow type="password" onChange={(e: { target: { value: React.SetStateAction<string>; }; })=>setPassword(e.target.value)} onKeyPress={handleOnKeyPress} />
+            <AnswerRow type="password" 
+             placeholder="비밀번호를 입력해주세요."
+            onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setPassword(e.target.value)} onKeyPress={handleOnKeyPress} />
           </RowArea>
 
         </LoginArea>
