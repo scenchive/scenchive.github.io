17ad243202b3ee686a07ef12100f61535343c3f8
jeongeun <wjddms9921@gmail.com>
2024-01-24 22:04:18 +0900

    DESIGN : Home 페이지 (로그인 O) 퍼블리싱

--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -8,120 +8,32 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   position: relative;
-  min-width: 992px;
-`;
-
-
-export const Top = styled.div`
-  label: top;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 130px;
-  position: relative;
-  margin-bottom: 50px;
-`;
-
-export const Search = styled.div<{ isSearching: boolean }>`
-  label: search;
-  width: 80%;
-  position: relative;
-
-  .search__input {
-    border: ${(props) =>
-      props.isSearching ? "1.5px #dabdff solid" : "1px #dabdff solid"};
-    border-radius: 30px;
-    height: 50px;
-    width: 100%;
-    padding: 0 20px;
-    box-sizing: border-box;
-    border-bottom-left-radius: ${(props) => (props.isSearching ? "0" : "30px")};
-    border-bottom-right-radius: ${(props) =>
-      props.isSearching ? "0" : "30px"};
-  }
-  .search__input::placeholder {
-    color: #b2b2b2;
-    font-size: 15px;
-  }
-  .search__input:focus {
-    outline: ${(props) =>
-      props.isSearching ? "0px #dabdff solid" : "0.5px #dabdff solid"};
-  }
-  .search__img {
-    width: 20px;
-    height: 20px;
-    position: absolute;
-    right: 15px;
-    top: 15px;
-  }
-`;
-
-export const SearchList = styled.div`
-  label: search-list;
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-  width: 80%;
-  height: fit-content;
-  border: 1.5px #dabdff solid;
-  border-top: none;
-  background-color: white;
-  padding: 14px;
-  box-sizing: border-box;
-  border-bottom-left-radius: 30px;
-  border-bottom-right-radius: 30px;
-  top: 50px;
-  z-index: 9999;
-  font-size: 15px;
-`;
-
-export const ListContent = styled.div`
-  label: list-content;
-  width: 100%;
-  .list-content__title {
-    width: fit-content;
-    color: #9a9a9a;
-  }
-  margin-bottom: 10px;
-`;
-
-export const ListDetail = styled.div`
-  label: list-detail;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 7px;
-  img {
-    width: 15px;
-  }
-  .list-detail__name {
-    width: 93%;
-    text-align: start;
-    color: #b2b2b2;
-  }
 `;
 
 export const Main = styled.div`
   label: main;
   width: 60%;
+  max-width: 700px;
+  height: calc(100vh - 170px);
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
+  font-family: Gowun Batang;
 `;
 
 export const MainTop = styled.div`
   label: main-top;
-  display: flex;
+  width: 100%;
   height: 40px;
-  font-family: NanumSquareRound;
-  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  font-size: 3rem;
+  font-weight: 700;
   align-items: start;
+  word-wrap: break-word;
   .main-top__text {
-    color: #c597ff;
+    color: #e3a6a1;
     line-height: 42px;
   }
   margin-bottom: 50px;
@@ -129,22 +41,29 @@ export const MainTop = styled.div`
 
 export const Select = styled.div`
   label: select;
-  font-family: NanumSquareRound;
-  font-size: 20px;
-  width: 80px;
+  width: 100px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 20px;
+  color: #bc5f6a;
+`;
+
+export const Selected = styled.div`
+  label: selected;
+  width: 100%;
+  font-size: 2.5rem;
   height: fit-content;
-  margin: 0 10px;
   position: relative;
-  border: 1px solid #dedddd;
-  border-radius: 5px;
-  padding: 3px 5px 3px 0;
-  background-color: white;
-  img {
-    width: 12px;
-    position: absolute;
-    top: 15px;
-    right: 7px;
-  }
+  border: 1px solid #bc5f6a;
+  border-radius: 10px;
+`;
+
+export const Options = styled.div`
+  label: options;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.5rem;
 `;
 
 export const Option = styled.div`
@@ -157,8 +76,9 @@ export const MainBottom = styled.div`
   label: main-bottom;
   width: 100%;
   display: flex;
+  font-family: Noto Sans KR;
   & > img {
-    width: 15px;
+    width: 20px;
   }
 `;
 
@@ -184,6 +104,7 @@ export const ContentText = styled.div`
   .content-text__name {
     font-size: 20px;
     margin-bottom: 10px;
+    text-align: start;
   }
   .content-text__brand-kr {
     font-size: 15px;
@@ -196,7 +117,7 @@ export const ContentText = styled.div`
   .content-text__rate {
     img {
       margin-right: 5px;
-      width: 15px;
+      width: 20px;
     }
     display: flex;
     margin-top: 10px;
