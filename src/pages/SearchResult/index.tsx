41528ce1148d2129627bf28ae63fd3b83a5b9dc1
jeongeun <wjddms9921@gmail.com>
2024-03-27 15:52:29 +0900

    FEAT : SearchResult Loading 추가

--- src/pages/SearchResult/index.tsx
+++ src/pages/SearchResult/index.tsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Container, Main, Content, List, Lists, ListText } from "./styles";
+import {
+  Container,
+  Main,
+  Content,
+  List,
+  Lists,
+  ListText,
+  Loading,
+} from "./styles";
 import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import { api } from "../../api";
 import Header from "../../components/Header";
@@ -28,6 +36,7 @@ const SearchResult = () => {
     null
   );
   const [perfumesPage, setPerfumesPage] = useState(0);
+  const [resultLoading, setResultLoading] = useState(true);
   const [loading, setLoading] = useState(false);
 
   const [target, setTarget] = useState<HTMLDivElement | null>(null);
@@ -82,6 +91,7 @@ const SearchResult = () => {
           else setResultBrands(res.data.brands);
           if (res.data.perfumesNum === 0) setResultPerfumes(null);
           else setResultPerfumes(res.data.perfumes);
+          setResultLoading(false);
         } else if (type === "more") {
           //무한스크롤
           if (res.data) {
@@ -100,70 +110,83 @@ const SearchResult = () => {
     <Container>
       <Header />
       <Search />
-      <Main>
-        {resultBrands !== null && (
-          <Content>
-            <div className="content__title">브랜드</div>
-            <Lists>
-              {resultBrands.map((el, index) => {
-                return (
-                  <List
-                    key={"brand_" + index}
-                    onClick={() =>
-                      navigate(`/branddetail?name=${el.brandName}`, {
-                        state: {
-                          brandName_kr: el.brandName_kr,
-                          brandImage: el.brandImage,
-                        },
-                      })
-                    }
-                  >
-                    <img src={el.brandImage} />
-                    <ListText>
-                      <div className="list-text__title">{el.brandName_kr}</div>
-                      <div className="list-text__sub-title">{el.brandName}</div>
-                    </ListText>
-                  </List>
-                );
-              })}
-            </Lists>
-          </Content>
-        )}
-        {resultPerfumes !== null && (
-          <Content>
-            <div className="content__title">향수</div>
-            <Lists ref={view}>
-              {resultPerfumes?.map((el, index) => {
-                return (
-                  <List
-                    key={"perfume_" + index}
-                    onClick={() =>
-                      navigate(`/perfumedetail?perfume=${el.perfumeId}`)
-                    }
-                  >
-                    <img src={`${el.perfumeImage}`} />
-                    <ListText>
-                      <div className="list-text__title">{el.perfumeName}</div>
-                      <div className="list-text__sub-title">{el.brandName}</div>
-                      <div className="list-text__sub-title">
-                        {el.brandName_kr}
-                      </div>
-                    </ListText>
-                  </List>
-                );
-              })}
-              {resultPerfumes && (
-                <div ref={setTarget} style={{ width: "100%", height: "1px" }} />
-              )}
-            </Lists>
-          </Content>
-        )}
-        {resultBrands === null && resultPerfumes === null && (
-          <Content>
-            <div className="content__none">검색결과가 없습니다.</div>
-          </Content>
-        )}
-      </Main>
+      {resultLoading ? (
+        <Loading>LOADING...</Loading>
+      ) : (
+        <Main>
+          {resultBrands !== null && (
+            <Content>
+              <div className="content__title">브랜드</div>
+              <Lists>
+                {resultBrands.map((el, index) => {
+                  return (
+                    <List
+                      key={"brand_" + index}
+                      onClick={() =>
+                        navigate(`/branddetail?name=${el.brandName}`, {
+                          state: {
+                            brandName_kr: el.brandName_kr,
+                            brandImage: el.brandImage,
+                          },
+                        })
+                      }
+                    >
+                      <img src={el.brandImage} />
+                      <ListText>
+                        <div className="list-text__title">
+                          {el.brandName_kr}
+                        </div>
+                        <div className="list-text__sub-title">
+                          {el.brandName}
+                        </div>
+                      </ListText>
+                    </List>
+                  );
+                })}
+              </Lists>
+            </Content>
+          )}
+          {resultPerfumes !== null && (
+            <Content>
+              <div className="content__title">향수</div>
+              <Lists ref={view}>
+                {resultPerfumes?.map((el, index) => {
+                  return (
+                    <List
+                      key={"perfume_" + index}
+                      onClick={() =>
+                        navigate(`/perfumedetail?perfume=${el.perfumeId}`)
+                      }
+                    >
+                      <img src={`${el.perfumeImage}`} />
+                      <ListText>
+                        <div className="list-text__title">{el.perfumeName}</div>
+                        <div className="list-text__sub-title">
+                          {el.brandName}
+                        </div>
+                        <div className="list-text__sub-title">
+                          {el.brandName_kr}
+                        </div>
+                      </ListText>
+                    </List>
+                  );
+                })}
+                {resultPerfumes && (
+                  <div
+                    ref={setTarget}
+                    style={{ width: "100%", height: "1px" }}
+                  />
+                )}
+              </Lists>
+            </Content>
+          )}
+          {resultBrands === null && resultPerfumes === null && (
+            <Content>
+              <div className="content__none">검색결과가 없습니다.</div>
+            </Content>
+          )}
+        </Main>
+      )}
     </Container>
   );
 };
